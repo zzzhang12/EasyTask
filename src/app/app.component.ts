a2336cf1dfbedfc88d0a602f766321dec296b300
zzzhang12 <zhangz_12@outlook.com>
2025-07-03 22:09:36 +0100

    feat: reuse user component and use custom component inputs to display multiple users.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { HeaderComponent } from './header/header.component';
 
 import { UserComponent } from './user/user.component';
 
+import { DUMMY_USERS } from './dummy-users';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +13,6 @@ import { UserComponent } from './user/user.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
